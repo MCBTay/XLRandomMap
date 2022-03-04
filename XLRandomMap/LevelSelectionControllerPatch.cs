53266d12cacb8b1d413033d14f211f3e434e5f75
mcbtay <mcbtay@gmail.com>
2022-03-03 22:26:19 -0500

    added a patch for level selection controller

---
+++ XLRandomMap/LevelSelectionControllerPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HarmonyLib;
+
+namespace XLRandomMap
+{
+    public static class LevelSelectionControllerPatch
+    {
+        [HarmonyPatch(typeof(LevelSelectionController), "Update")]
+        public static class UpdatePatch
+        {
+            static void Postfix(LevelSelectionController __instance)
+            {
+                var player = PlayerController.Instance.inputController.player;
+
+                if (!player.GetButtonDown("X")) return;
+
+                var random = new Random();
+                var index = random.Next(LevelManager.Instance.ModLevels.Count);
+
+                LevelManager.Instance.PlayLevel(LevelManager.Instance.ModLevels[index]);
+            }
+        }
+    }
+}
